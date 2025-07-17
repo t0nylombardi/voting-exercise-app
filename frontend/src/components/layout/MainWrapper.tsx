b6a07a54171ef5a9efcbad960f5a1a2f481bba1d
t0nylombardi <alombardi.331@gmail.com>
2025-07-17 01:51:17 -0400

    feat: Update ResultsPage to use MainWrapper component
    Refactor ResultsPage to include MainWrapper for consistent layout.
    Add candidate data and display vote tallies in a structured format.

--- frontend/src/components/layout/MainWrapper.tsx
+++ frontend/src/components/layout/MainWrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const MainWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex items-start justify-start min-h-screen m-20">
-      <div className="bg-white p-6 w-[42rem]">{children}</div>
+      <div className="bg-white p-6 w-full">{children}</div>
     </div>
   );
 };
