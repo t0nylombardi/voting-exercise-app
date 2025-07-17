85bd39eeb0d26a3a66e749f0c46e3df4cc1ba050
t0nylombardi <alombardi.331@gmail.com>
2025-07-17 13:08:58 -0400

    feat: Improve layout and styling of NavBar and VotePage

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -9,15 +9,15 @@ interface NavBarProps {
 
 export default function NavBar({ user, isLoggedIn, logout }: NavBarProps) {
   return (
-    <nav className="flex items-center justify-between px-6 py-4 border-4 border-black/50 ">
-      <h1 className="text-xl">VOTE.WEBSITE</h1>
+    <nav className="flex items-center justify-between px-6 py-4 border-4 border-black/50">
+      <h1 className="text-xl flex-1">VOTE.WEBSITE</h1>
 
       {isLoggedIn && user && (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 whitespace-nowrap">
           <span className="text-lg text-right block">
             Signed in as <span className="font-medium">{user.email}</span>
           </span>
-          <Button onClick={logout} className="w-24">
+          <Button onClick={logout} className="w-auto text-sm py-4 px-4">
             Logout
           </Button>
         </div>
--- frontend/src/pages/VotePage.tsx
+++ frontend/src/pages/VotePage.tsx
@@ -24,57 +24,62 @@ const VotePage = (): ReactElement => {
 
   return (
     <MainWrapper>
-      <h1 className="text-6xl leading-[1.5] mb-12">Cast your vote today!</h1>
+      <div className="w-[45rem]">
+        <h1 className="text-6xl leading-[1.5] mb-12">Cast your vote today!</h1>
 
-      <form onSubmit={handleVote} className="space-y-6 w-full">
-        <fieldset className="space-y-3">
-          {candidates.map((name) => (
-            <label key={name} className="flex items-center text-2xl space-x-4">
-              <input
-                type="radio"
-                name="vote"
-                value={name}
-                checked={selectedCandidate === name}
-                onChange={() => {
-                  setSelectedCandidate(name);
-                  setWriteIn("");
-                }}
-                className={clsx(
-                  "appearance-none w-10 h-10 my-4 rounded-full border-4 border-black/50",
-                  selectedCandidate === name && "bg-black/50"
-                )}
-              />
-              <span>{name}</span>
-            </label>
-          ))}
-          <Button type="submit" className=" w-1/4 my-4 py-4 text-4xl">
-            Vote
-          </Button>
-        </fieldset>
-
-        <hr className="border-t border-black/50 border-2 my-12" />
-
-        <div>
-          <label htmlFor="write-in" className="block text-2xl mb-6">
-            Or, add a new candidate:
-          </label>
-          <div className="flex flex-row items-center gap-4">
-            <TextInput
-              name="write-in"
-              value={writeIn}
-              onChange={(e) => {
-                setWriteIn(e.target.value);
-                setSelectedCandidate("");
-              }}
-              placeholder="Enter name..."
-              className="flex-1 py-5 px-4 text-2xl border-4 border-black/50 rounded-xl"
-            />
-            <Button type="submit" className="text-medium w-32 py-6 text-2xl">
+        <form onSubmit={handleVote} className="space-y-6 w-full">
+          <fieldset className="space-y-3">
+            {candidates.map((name) => (
+              <label
+                key={name}
+                className="flex items-center text-2xl space-x-4"
+              >
+                <input
+                  type="radio"
+                  name="vote"
+                  value={name}
+                  checked={selectedCandidate === name}
+                  onChange={() => {
+                    setSelectedCandidate(name);
+                    setWriteIn("");
+                  }}
+                  className={clsx(
+                    "appearance-none w-10 h-10 my-4 rounded-full border-4 border-black/50",
+                    selectedCandidate === name && "bg-black/50"
+                  )}
+                />
+                <span>{name}</span>
+              </label>
+            ))}
+            <Button type="submit" className=" w-1/4 my-4 py-4 text-4xl">
               Vote
             </Button>
+          </fieldset>
+
+          <hr className="border-t border-black/50 border-2 my-12" />
+
+          <div>
+            <label htmlFor="write-in" className="block text-2xl mb-6">
+              Or, add a new candidate:
+            </label>
+            <div className="flex flex-row items-center gap-4">
+              <TextInput
+                name="write-in"
+                value={writeIn}
+                onChange={(e) => {
+                  setWriteIn(e.target.value);
+                  setSelectedCandidate("");
+                }}
+                placeholder="Enter name..."
+                className="flex-1 py-5 px-4 text-2xl border-4 border-black/50 rounded-xl"
+              />
+              <Button type="submit" className="text-medium w-32 py-6 text-2xl">
+                Vote
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </MainWrapper>
   );
 };
